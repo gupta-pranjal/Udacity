cafd45181f2f06ce60c3fe3ef254cc24123159d6
root <pranjal.g@outlook.com>
2020-12-09 22:47:12 +0000

    first commit for project1

---
+++ build/CMakeFiles/Welcome_message.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Welcome_message.dir/script/Welcome_message.cpp.o"
+  "libWelcome_message.pdb"
+  "libWelcome_message.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Welcome_message.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
